6a476200acbab49c4f708a763d1076e99876c148
Edson Moreira <w.moreirae@gmail.com>
2020-06-17 21:15:19 -0300

    Mission Acomplished

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,77 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const new_repo = {
+    id: uuid(),
+    title: title,
+    url: url,
+    techs: techs,
+    likes: 0,
+  }
+
+  repositories.push(new_repo);
+  return response.json(new_repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs} = request.body;
+
+  if (!isUuid(id)){
+    return response.status(400).json({message: "Requested ID is not a valid ID"});
+  }
+
+  const repo = repositories.find(e => {return id == e.id});
+  if (repo === undefined){
+    return response.status(404).json({message: "No repository with the requested ID was found"});
+  }
+
+  repo.title = title;
+  repo.url = url;
+  repo.techs = techs;
+
+  return response.json(repo);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  if (!isUuid(id)){
+    return response.status(400).json({message: "Requested ID is not a valid ID"});
+  }
+
+  const repo = repositories.find(e => {return id == e.id});
+  if (repo === undefined){
+    return response.status(404).json({message: "No repository with the requested ID was found"});
+  }
+
+  const index = repositories.indexOf(repo)
+  repositories.splice(index, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  if (!isUuid(id)){
+    return response.status(400).json({message: "Requested ID is not a valid ID"});
+  }
+
+  const repo = repositories.find(e => {return id == e.id});
+  if (repo === undefined){
+    return response.status(404).json({message: "No repository with the requested ID was found"});
+  }
+
+  repo.likes += 1
+
+  return response.json(repo);
 });
 
 module.exports = app;
